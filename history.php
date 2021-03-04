7d421f5902c762b68ebdb98741e030db03808a64
petagys <dack2702@rambler.ru>
2021-03-04 21:52:44 +0300

    history

--- history.php
+++ history.php
@@ -1,5 +1,7 @@
 <?php
 require_once 'db_connection.php';
+ini_set("display_errors", 1);
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
 
 $db = new Db();
 
@@ -18,7 +20,38 @@ if('GET' == $_SERVER['REQUEST_METHOD'] && isset($_GET['count'])){
 }
 
 if('POST' == $_SERVER['REQUEST_METHOD']){
-
+    $json = json_decode(file_get_contents('php://input'), true);
+    $SQL1 = "SELECT * FROM results WHERE ";
+    $conditions = [];
+    if($json['name']){
+        $conditions.array_push($conditions, "(`name` like '{$json['name']}%' or `surname` like '{$json['name']}%')");
+    }
+    if(!empty($json['email'])){
+        $conditions.array_push($conditions, "`email` like '%{$json['email']}%'");
+    }
+    if(!empty($json['from'])){
+        $conditions.array_push($conditions, "`date` >= '{$json['from']}'");
+    }
+    if(!empty($json['to'])){
+        $conditions.array_push($conditions, "`date` <= '{$json['to']}'");
+    }
+    foreach($conditions as $key => $val){
+        $SQL1 .= $val;
+        if($key !== count($conditions) - 1){
+            $SQL1 .= ' and ';
+        }
+    }
+    $result = array();
+    // echo $SQL1;
+    $dbResult = $db->SQLSelect($SQL1);
+    if (mysqli_num_rows($dbResult) > 0) {
+        while($row = mysqli_fetch_assoc($dbResult)) {
+            $row['results'] = json_decode($row['results']);
+            array_push($result, $row);
+        }
+    }
+    echo json_encode($result);
+    exit;
 }
 
 header('HTTP/1.1 500 Internal Server Error');
